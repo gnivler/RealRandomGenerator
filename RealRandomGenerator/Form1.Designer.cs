3633d225db539b7f8cd00e73f8f5a1c9811bd0b2
gnivler <gnivil@gmail.com>
2018-07-06 14:38:37 -0600

    initial commit

--- RealRandomGenerator/Form1.Designer.cs
+++ RealRandomGenerator/Form1.Designer.cs
@@ -28,13 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Go = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.max = new System.Windows.Forms.Label();
+            this.min = new System.Windows.Forms.Label();
+            this.avg = new System.Windows.Forms.Label();
+            this.lblAvg = new System.Windows.Forms.Label();
+            this.lblDrops = new System.Windows.Forms.Label();
+            this.lblmin = new System.Windows.Forms.Label();
+            this.drops = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Go
+            // 
+            this.Go.Location = new System.Drawing.Point(13, 12);
+            this.Go.Name = "Go";
+            this.Go.Size = new System.Drawing.Size(120, 23);
+            this.Go.TabIndex = 0;
+            this.Go.Text = "Go";
+            this.Go.UseVisualStyleBackColor = true;
+            this.Go.Click += new System.EventHandler(this.Go_click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(120, 168);
+            this.textBox1.TabIndex = 5;
+            // 
+            // max
+            // 
+            this.max.AutoSize = true;
+            this.max.Location = new System.Drawing.Point(59, 68);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(13, 13);
+            this.max.TabIndex = 3;
+            this.max.Text = "0";
+            // 
+            // min
+            // 
+            this.min.AutoSize = true;
+            this.min.Location = new System.Drawing.Point(59, 29);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(13, 13);
+            this.min.TabIndex = 3;
+            this.min.Text = "0";
+            // 
+            // avg
+            // 
+            this.avg.AutoSize = true;
+            this.avg.Location = new System.Drawing.Point(59, 102);
+            this.avg.Name = "avg";
+            this.avg.Size = new System.Drawing.Size(13, 13);
+            this.avg.TabIndex = 3;
+            this.avg.Text = "0";
+            // 
+            // lblAvg
+            // 
+            this.lblAvg.AutoSize = true;
+            this.lblAvg.Location = new System.Drawing.Point(6, 102);
+            this.lblAvg.Name = "lblAvg";
+            this.lblAvg.Size = new System.Drawing.Size(47, 13);
+            this.lblAvg.TabIndex = 2;
+            this.lblAvg.Text = "Average";
+            // 
+            // lblDrops
+            // 
+            this.lblDrops.AutoSize = true;
+            this.lblDrops.Location = new System.Drawing.Point(6, 141);
+            this.lblDrops.Name = "lblDrops";
+            this.lblDrops.Size = new System.Drawing.Size(35, 13);
+            this.lblDrops.TabIndex = 4;
+            this.lblDrops.Text = "Drops";
+            // 
+            // lblmin
+            // 
+            this.lblmin.AutoSize = true;
+            this.lblmin.Location = new System.Drawing.Point(6, 29);
+            this.lblmin.Name = "lblmin";
+            this.lblmin.Size = new System.Drawing.Size(24, 13);
+            this.lblmin.TabIndex = 2;
+            this.lblmin.Text = "Min";
+            // 
+            // drops
+            // 
+            this.drops.AutoSize = true;
+            this.drops.Location = new System.Drawing.Point(59, 141);
+            this.drops.Name = "drops";
+            this.drops.Size = new System.Drawing.Size(13, 13);
+            this.drops.TabIndex = 4;
+            this.drops.Text = "0";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(6, 68);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(27, 13);
+            this.lblMax.TabIndex = 2;
+            this.lblMax.Text = "Max";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblMax);
+            this.groupBox1.Controls.Add(this.drops);
+            this.groupBox1.Controls.Add(this.lblmin);
+            this.groupBox1.Controls.Add(this.lblDrops);
+            this.groupBox1.Controls.Add(this.lblAvg);
+            this.groupBox1.Controls.Add(this.avg);
+            this.groupBox1.Controls.Add(this.min);
+            this.groupBox1.Controls.Add(this.max);
+            this.groupBox1.Location = new System.Drawing.Point(142, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(234, 168);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Statistics";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(388, 226);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Go);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Real RNG";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Go;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label max;
+        private System.Windows.Forms.Label min;
+        private System.Windows.Forms.Label avg;
+        private System.Windows.Forms.Label lblAvg;
+        private System.Windows.Forms.Label lblDrops;
+        private System.Windows.Forms.Label lblmin;
+        private System.Windows.Forms.Label drops;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- RealRandomGenerator/Form1.cs
+++ RealRandomGenerator/Form1.cs
@@ -1,20 +1,112 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RealRandomGenerator
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random();
+        int minValue = int.MaxValue;
+        int maxValue = int.MinValue;
+        int maxRoll = 101;  // +1 for exclusive upper bound
+        int numRolls = 200;
+        int randomCeiling = int.MaxValue;
+        List<int> numbers = new List<int>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Go_click(object sender, EventArgs e)
+        {
+            ResetForm();
+            for (int i = 0; i < numRolls; i++)
+            {
+                var number = JustRNG();
+                //var number = GenerateRealRandom();
+                numbers.Add(number);
+                textBox1.Text = $"{textBox1.Text}{number.ToString()}{Environment.NewLine}";
+                if (isNewMin(number))
+                {
+                    min.Text = number.ToString();
+                    min.Update();
+                }
+                if (isNewMax(number))
+                {
+                    max.Text = number.ToString();
+                    max.Update();
+                }
+            }
+            avg.Text = numbers.Average().ToString();
+        }
+
+        private void ResetForm()
+        {
+            textBox1.Clear();
+            numbers.Clear();
+            min.Text = "0";
+            max.Text = "0";
+            avg.Text = "0";
+            minValue = int.MinValue;
+            maxValue = int.MaxValue;
+        }
+
+        public int JustRNG()
+        {
+            return rng.Next(1, maxRoll);
+        }
+
+        public int GenerateRealRandom()
+        {
+            int r = 0;
+            while (true)
+            {
+                r = rng.Next(1, maxRoll);
+                if (r < (randomCeiling) - (randomCeiling % r))
+                {
+                    return r % maxRoll;
+                }
+                else
+                {
+                    UpdateDrops();
+                }
+            }
+        }
+
+        public bool isNewMin(int number)
+        {
+            if (number < minValue)
+            {
+                minValue = number;
+                return true;
+            }
+            return false;
+        }
+
+        public bool isNewMax(int number)
+        {
+            if (number > maxValue)
+            {
+                maxValue = number;
+                return true;
+            }
+            return false;
+        }
+
+        private void UpdateDrops()
+        {
+            int dropNum = Convert.ToInt32(drops.Text.ToString());
+            dropNum++;
+            drops.Text = dropNum.ToString();
+            drops.Update();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
